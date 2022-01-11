4ca117ada40dd66b294af5306b89fd73d278228b
christoferson <sweetsorrow1981@yahoo.co.jp>
2022-01-11 22:34:37 +0900

    Variables and Values

--- 01-Basics/script.js
+++ 01-Basics/script.js
@@ -1,2 +1,19 @@
-var val = 18 + 24 - 17 +2 - 17;
-console.log("console.log: " + val);
+
+// Declare Variable
+let variable = "String Variable";
+console.log(variable);
+
+// Primitive Variable
+let age = 23; console.log(typeof age); //Number
+let name = "Peter"; console.log(typeof name);
+let flag = false; console.log(typeof flag);
+let count; console.log(typeof count); //Undefined
+let nothing = null; console.log(typeof nothing); //Null
+let symbol = Symbol.for("Spade"); console.log(typeof symbol); //Symbol
+let zillion = BigInt("9374320"); console.log(typeof zillion); //BigInt
+//Note: Dynamic Type - Data Types are inferred. Value has the Type. Variable only points to a Value.
+
+// Object Variable
+let me = {
+
+};
