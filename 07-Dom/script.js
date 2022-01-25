d4e664b67fd49575172364314700a44ed6373bdb
christoferson <sweetsorrow1981@yahoo.co.jp>
2022-01-25 18:43:10 +0900

    Add 07-Dom

---
+++ 07-Dom/script.js
@@ -0,0 +1,6 @@
+// Enable Strict Mode (Write more secure robust code)
+'use strict';
+
+// Query Selector
+console.log(document.querySelector(".message"));
+console.log(document.querySelector(".message").textContent);
