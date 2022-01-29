525e45368caf4ef44552bd8aab3f8190ff6ea963
christoferson <sweetsorrow1981@yahoo.co.jp>
2022-01-29 20:53:00 +0900

    Game Update

--- 07-Dom/script.js
+++ 07-Dom/script.js
@@ -5,17 +5,54 @@
 console.log(document.querySelector(".message"));
 console.log(document.querySelector(".message").textContent);
 
+document.querySelector(".message").textContent = "Yey!";
+console.log(document.querySelector(".message").textContent);
 
-let start = Date.now(); 
-console.log(start);
+console.log(document.querySelector(".guess").value);
+document.querySelector(".guess").value = 23;
 
-//start.setDays(start.getDate + 1);
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
+console.log(secretNumber);
+//document.querySelector(".number").textContent = secretNumber;
 
-const date = new Date();
-console.log(date);
-date.setFullYear(date.getFullYear() + 1);
-console.log(date, date.valueOf(), typeof date.valueOf());
+let currentScore = 10;
+document.querySelector(".score").textContent = currentScore;
 
+let highScore = 0;
+document.querySelector(".highscore").textContent = highScore;
 
-const TTL_DELTA = 60 * 60 * 24 * 7; // Keep records for 7 days
-console.log((Math.floor(+new Date() / 1000) + TTL_DELTA).toString())
+// Handling Clicks
+document.querySelector(".check").addEventListener("click", function() {
+    const messageElement = document.querySelector(".message");
+    const guessElement = document.querySelector(".guess");
+    const scoreElement = document.querySelector(".score");
+
+    const guess = Number(guessElement.value);
+    if (!guess) {
+        console.log("Enter a number");
+        return;
+    }
+
+    if (guess === secretNumber) {
+        messageElement.textContent = "Correct!";
+        if (currentScore > highScore) {
+            highScore = currentScore;
+            document.querySelector(".highscore").textContent = highScore;
+        }
+    } else {
+        // Give Hint
+        if (guess > secretNumber) {
+            messageElement.textContent = "Too High";
+        } else {
+            messageElement.textContent = "Too Low";
+        }
+        
+        // Check and Update Score
+        if (currentScore <= 0) {
+            messageElement.textContent = "You lose";
+        } else {
+            currentScore--;
+        }
+        scoreElement.textContent = currentScore;
+    }
+});
