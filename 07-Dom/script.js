98ac2a1cd8cc0b200b7bd3b7e24097714e6a385d
christoferson <sweetsorrow1981@yahoo.co.jp>
2022-01-29 18:08:40 +0900

    Query Selector

--- 07-Dom/script.js
+++ 07-Dom/script.js
@@ -4,3 +4,18 @@
 // Query Selector
 console.log(document.querySelector(".message"));
 console.log(document.querySelector(".message").textContent);
+
+
+let start = Date.now(); 
+console.log(start);
+
+//start.setDays(start.getDate + 1);
+
+const date = new Date();
+console.log(date);
+date.setFullYear(date.getFullYear() + 1);
+console.log(date, date.valueOf(), typeof date.valueOf());
+
+
+const TTL_DELTA = 60 * 60 * 24 * 7; // Keep records for 7 days
+console.log((Math.floor(+new Date() / 1000) + TTL_DELTA).toString())
