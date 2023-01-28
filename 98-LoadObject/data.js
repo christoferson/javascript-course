a69ba38e990b7d1528781bffc3d4c35a3200c0e1
christoferson <sweetsorrow1981@yahoo.co.jp>
2023-01-28 22:00:38 +0900

    Load Object

---
+++ 98-LoadObject/data.js
@@ -0,0 +1,40 @@
+var users =
+
+    [
+        {
+            "name": "Mark"
+        },
+
+        {
+            "name": "Luke"
+        }
+    ]
+;
+
+var catalog = {
+    products: [
+        { product1: [11, 12, 13, 14, 15] },
+        { product2: [21, 22, 23] },
+        { product3: [31, 32] }
+    ]
+};
+
+
+var products = {
+    "product1": {
+        "name": "product-a",
+        "price": "98.28",
+        "currency": "USD",
+    },
+    "product2": {
+        "name": "product-b",
+        "price": "35.78",
+        "currency": "USD",
+    },
+    "product3": {
+        "name": "product-c",
+        "price": "35.78",
+        "currency": "USD",
+    }
+};
+  
