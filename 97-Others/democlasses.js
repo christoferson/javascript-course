bd1355e3f48d24d2a883a7acd12e2da1d659e5e0
christoferson <sweetsorrow1981@yahoo.co.jp>
2023-01-31 00:04:10 +0900

    Add Class Methods

--- 97-Others/democlasses.js
+++ 97-Others/democlasses.js
@@ -1,11 +1,35 @@
+'use strict;'
+
+// Properties
+// ES6 properties declared inside constructor using this keyword
+// ES7 properties declared directly inside the class.
+
+// Methods
+// ES6 function declared using regular syntax
+// ES7 function declared using arrow notation
+
 class Character {
+    
+    vitality = 99;
+    
     constructor(id) {
         this.id = id;
-    }
+    };
+    
+    emote() {
+      console.log(`Character ${this.id} emotes.`);
+    };
+    
+    wave = () => {
+      console.log(`Character ${this.id} waves.`);
+    };
+    
 };
 
 const character = new Character("Sabrina");
-console.log(`Name: ${character.id}`);
+console.log(`Name: ${character.id} Vitality: ${character.vitality}`);
+character.emote();
+character.wave();
 
 class Wizard extends Character {
 
@@ -17,5 +41,5 @@ class Wizard extends Character {
 };
 
 const wizard = new Wizard("Willow", 1028);
-console.log(`Name: ${wizard.id} Mana: ${wizard.mana}`);
+console.log(`Name: ${wizard.id} Vitality: ${wizard.vitality} Mana: ${wizard.mana}`);
 
