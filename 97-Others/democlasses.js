9755b3a15e2e86d6f6b8b6572fd3e8fd8c29260b
christoferson <sweetsorrow1981@yahoo.co.jp>
2023-01-31 14:16:34 +0900

    Demo Class Update

--- 97-Others/democlasses.js
+++ 97-Others/democlasses.js
@@ -1,45 +1,34 @@
 'use strict;'
 
-// Properties
-// ES6 properties declared inside constructor using this keyword
-// ES7 properties declared directly inside the class.
+// Array Destructuring
 
-// Methods
-// ES6 function declared using regular syntax
-// ES7 function declared using arrow notation
+const [a, b] = ["A", "B", "C"];
+console.log(a);
+console.log(b);
+
+const [a2, ,c2] = ["A", "B", "C"]; // Skip B
+console.log(a2, c2);
+
+// Object Destructuring
+
+const {name, age} = {
+  "name": "Mark",
+  "age": 24,
+  "occupation": "Apostle"
+};
+
+console.log(name);
+console.log(age);
+
+// Class Destructuring
 
 class Character {
-    
-    vitality = 99;
-    
-    constructor(id) {
-        this.id = id;
-    };
-    
-    emote() {
-      console.log(`Character ${this.id} emotes.`);
-    };
-    
-    wave = () => {
-      console.log(`Character ${this.id} waves.`);
-    };
-    
+    id = "Luke";
+    vitality = 98;
+    attack = 12;
 };
 
-const character = new Character("Sabrina");
-console.log(`Name: ${character.id} Vitality: ${character.vitality}`);
-character.emote();
-character.wave();
-
-class Wizard extends Character {
-
-    constructor(id, mana) {
-        super(id);
-        this.mana = mana;
-    }
-
-};
-
-const wizard = new Wizard("Willow", 1028);
-console.log(`Name: ${wizard.id} Vitality: ${wizard.vitality} Mana: ${wizard.mana}`);
-
+const luke = new Character();
+console.log(luke);
+const {id, vitality} = luke;
+console.log(id, vitality);
